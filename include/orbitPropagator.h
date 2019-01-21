91ef73bf4a26a89a68b2446e7cb70b7037807aac
James Bell <jtb2013@gmail.com>
2019-01-21 17:19:46 -0600

    more compile fixes

--- include/orbitPropagator.h
+++ include/orbitPropagator.h
@@ -4,8 +4,8 @@
 #ifndef ORBIT_PROPAGATOR_H
 #define ORBIT_PROPAGATOR_H
 
-void cwProp(Eigen::MatrixXd& stateHist, const Eigen::Vector3d& r0, const Eigen::Vector3d& v0, const Eigen::MatrixXd& control, const double& tf, const int& intervals, const Params& p);
+void cwProp(Eigen::MatrixXd& stateHist, const Eigen::Vector3d& r0, const Eigen::Vector3d& v0, const Eigen::MatrixXd& control, const double& tf, const int& intervals, const OrbitalParams& p);
 
-Eigen::VectorXd cwDeriv(const double& t, const Eigen::VectorXd& state, const Eigen::VectorXd& u, const Params& p);
+Eigen::VectorXd cwDeriv(const double& t, const Eigen::VectorXd& state, const Eigen::VectorXd& u, const OrbitalParams& p);
 
 #endif
